75384089bc256d75f4f96e7df97724d347f37b44
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-01-30 17:55:18 +0000

    Initial commit and added Express, dotenv and nodemon, and dummy products data

---
+++ data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Kettlebell - Tiger (Blue)',
+    image: '/images/kettlebell_animals/1.jpg',
+    description:
+      'The "Kettlebell - Tiger (Blue)" combines a striking blue color with a captivating tiger design, offering both aesthetic appeal and functionality for strength and conditioning workouts with its durable construction and ergonomic design.',
+    brand: 'Kettlebell Gear',
+    category: 'Animals',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Kettlebell - Bear (Dark Yellow)',
+    image: '/images/kettlebell_animals/2.jpg',
+    description:
+      'Kettlebell - Bear (Dark Yellow)',
+    brand: 'Kettlebell Gear',
+    category: 'Animals',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Kettlebell - Tiger (Dark Blue)',
+    image: '/images/kettlebell_animals/3.jpg',
+    description:
+      'Kettlebell - Tiger (Dark Blue)',
+    brand: 'Kettlebell Gear',
+    category: 'Animals',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Kettlebell - Lion (Yellow)',
+    image: '/images/kettlebell_animals/4.jpg',
+    description:
+      'Kettlebell - Lion (Yellow)',
+    brand: 'Kettlebell Gear',
+    category: 'Animals',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Kettlebell - Bear (Yellow + Blue)',
+    image: '/images/kettlebell_animals/5.jpg',
+    description:
+      'Kettlebell - Bear (Yellow + Blue)',
+    brand: 'Kettlebell Gear',
+    category: 'Animals',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Kettlebell - Tiger (Dark+ Yellow + Blue)',
+    image: '/images/kettlebell_animals/6.jpg',
+    description:
+      'Kettlebell - Tiger (Dark+ Yellow + Blue)',
+    brand: 'Kettlebell Gear',
+    category: 'Animals',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
