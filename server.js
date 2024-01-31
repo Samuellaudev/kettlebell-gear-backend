ddc61bb8ed92c16001c6d055b06ada0e6f1a65fe
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-01-31 00:18:28 +0000

    Added productRoutes and asyncHandler

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
-import products from './data/products.js';
 import connectDB from './config/db.js';
 import colors from 'colors'
+import productRoutes from './routes/productRoutes.js';
+
 
 connectDB();
 
@@ -15,14 +16,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 
 app.listen(port, () => console.log(colors.yellow(`Server running on port ${port}`)));
