d54fa2570e4a9172b2ec69acf5d33a0dc22b74ad
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-01-31 16:00:01 +0000

    Backend Authentication Implementation

---
+++ utils/generateToken.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken'
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1d'})
+
+  // Set JWT as HTTP-Only cookie
+  res.cookie('jwt', token, { 
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+    sameSite: 'strict', // Prevent CSRF attacks
+    maxAge: 24 * 60 * 60 * 1000 // 1 Day
+  })
+}
+
+export default generateToken
