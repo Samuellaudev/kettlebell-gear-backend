24f27d0fdd1acb7fa07174b9dfe18be5ead0d367
Samuel <76025791+Samuellaudev@users.noreply.github.com>
2024-02-01 18:14:59 +0000

    Added controller functions for create-product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,10 +3,14 @@ const router = express.Router()
 import {
   getProducts,
   getProductById,
+  createProduct
 } from '../controllers/productController.js';
 import checkObjectId from '../middleware/checkObjectId.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.route('/').get(getProducts);
+router.route('/')
+  .get(getProducts)
+  .post(protect, admin, createProduct);
 router
   .route('/:id')
   .get(checkObjectId, getProductById)
